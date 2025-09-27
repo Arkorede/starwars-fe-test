1f95ce181565485ad0acaedb2753b6a2c21585bc
Arkorede <israelashaolu2000@gmail.com>
2025-09-27 23:48:57 +0100

    fix: deployment error

--- src/app/components/tables/film-table.tsx
+++ src/app/components/tables/film-table.tsx
@@ -43,7 +43,7 @@ export default function FilmTable() {
     <>
       <DataTable<Film>
         title="Films"
-        data={films.results || []}
+        data={films?.results || []}
         columns={filmsColumns}
         onRowClick={handleViewFilm}
       />
